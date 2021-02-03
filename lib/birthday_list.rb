ea3cf91ac6388886a99a1c5a7d3fb2ec580f25e1
olga <itsglimerry@gmail.com>
2021-02-03 22:29:15 +0000

    Test and code added for method .add and initialized class BirthdayList

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,10 @@
+class BirthdayList
+  def initialize
+    @birthdays = []
+  end
 
+  def add name,date
+    birthday = {:names => name, :dates => date }
+    @birthdays.push(birthday)
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,14 @@
+require 'birthday_list'
 
+
+describe BirthdayList do
+birthdays = @birthdays
+birthday_list = BirthdayList.new
+
+
+describe '#add' do
+  it 'adds a birthday to the list' do
+    expect(birthday_list.add('name','date')).to eq   ([{:dates=>"date", :names=>"name"}])
+    end
+  end
+end
