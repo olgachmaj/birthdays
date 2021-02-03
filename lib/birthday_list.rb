f5b5b17a2d4399a226c27784b5977ab15779037c
olga <itsglimerry@gmail.com>
2021-02-03 22:36:20 +0000

    Added a test that throws an error if there is no birthdays to showand test checking if .show_birthdays is printing right output. Implemented .show_birthdays  method. Added draft test of .today method.

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -7,4 +7,24 @@ class BirthdayList
     birthday = {:names => name, :dates => date }
     @birthdays.push(birthday)
   end
+
+
+
+  def show_birthdays
+    if @birthdays == []
+      raise'No birthdays avalible!'
+    end
+
+    list_to_print = ''
+
+    @birthdays.each do |b|
+      print "#{b[:names]}, #{b[:dates]} \n"
+    end
+
+    @birthdays.each do |b|
+      list_to_print += "#{b[:names]}, #{b[:dates]}\n"
+    end
+
+    list_to_print
+  end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -11,4 +11,23 @@ describe '#add' do
     expect(birthday_list.add('name','date')).to eq   ([{:dates=>"date", :names=>"name"}])
     end
   end
-end
+
+  describe '#show_birthdays' do
+    it 'throws an error when there is no birthdays' do
+      bday_list = BirthdayList.new
+      expect{bday_list.show_birthdays}.to raise_error('No birthdays avalible!')
+    end
+
+    it 'prints all birthdays' do
+      expect(birthday_list.show_birthdays).to eq "name, date\n"
+    end
+  end
+
+  #describe '#today' do
+    #it 'shows who is celebrating their bday today' do
+      #double = BirthdayList.new
+      #double.add('terry','12')
+      #todayis = 12
+      #expect(birthday_list.today(todayis)).to eq 'terry'
+  #  end
+  end
